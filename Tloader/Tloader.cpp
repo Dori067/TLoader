a58bc51ff7387d83ea03c0e46db419ec93b33ebc
Dornotgame <76527614+Dornotgame@users.noreply.github.com>
2024-11-15 19:43:12 +0100

    somethings

--- Tloader/Tloader.cpp
+++ Tloader/Tloader.cpp
@@ -9,7 +9,8 @@
 
 namespace fs = std::filesystem;
 
-int movef() {
+int movef(bool isjava, bool istl, bool isclient) {
+
     std::locale::global(std::locale(""));
 
     std::string javaf = "javat";
@@ -63,42 +64,43 @@ int setdir() {
 int main() {
     std::locale::global(std::locale(""));
     int num;
-    int ifjava;
-    int iftl;
-    int ifclient;
+    bool ifjava;
+    bool iftl;
+    bool ifclient;
     fs::path current_dir = fs::current_path();
     fs::path java_dir = current_dir / "java";
     fs::path client_dir = current_dir / ".minecraft";
     fs::path tl_dir = current_dir / ".tlauncher";
 
+
     if (fs::exists(java_dir) && fs::is_directory(java_dir)) {
         std::cout << "Java Found \n";
-        ifjava = 1;
+        ifjava = true;
     }
     else {
         std::cout << "Java Not Found \n";
-        ifjava = 0;
+        ifjava = false;
     };
 
     if (fs::exists(tl_dir) && fs::is_directory(tl_dir)) {
         std::cout << "Tl Found \n";
-        iftl = 1;
+        iftl = true;
     }
     else {
         std::cout << "Tl Not Found \n";
-        iftl = 0;
+        iftl = false;
     };
 
     if (fs::exists(client_dir) && fs::is_directory(client_dir)) {
         std::cout << "Client Found \n";
-        ifclient = 1;
+        ifclient = true;
     }
     else {
         std::cout << "Client Not Found \n";
-        ifclient = 0;
+        ifclient = false;
     };
     setdir();
-    movef();
+    movef(ifjava, iftl, ifclient);
 
     std::cout << ifjava << iftl << ifclient;
     std::cin >> num;
